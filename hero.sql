f1b5ca0d9001bcf4c21f6b42e07e68548b61e135
Mougn <99721988+Mougn@users.noreply.github.com>
2022-04-11 12:58:13 +0200

    fix: requet

--- hero.sql
+++ hero.sql
@@ -1,3 +1,30 @@
  /*Les requêtes sql*/
-SELECT * FROM Heros
-ORDER BY Nom DESC;
+/**/
+SELECT Nom, PV FROM Héros 
+WHERE PV = 40;
+/**/
+SELECT COUNT(Nom) FROM Héros 
+WHERE PV = 40;
+/**/
+SELECT Nom, PV FROM Héros 
+ORDER BY PV ASC;
+/**/
+SELECT Nom, MAX(PV) FROM Héros;
+/**/
+SELECT SUM(PV) as "Somme des PVs des personnages" FROM Héros;
+/**/
+SELECT Heros.Nom, CompetenceHeros.Nom FROM CompetenceHeros
+JOIN Heros
+ON Heros.ID = CompetenceID;
+/**/
+SELECT Heros.Nom, Arme.Nom FROM Heros
+JOIN Arme
+ON Heros.ID = Arme.HerosID;
+/**/
+SELECT DISTINCT Heros.Nom, Arme.Nom, CompetenceHeros.Nom FROM Heros
+INNER JOIN Arme
+INNER JOIN CompetenceHeros
+ON CompetenceHeros.HerosID = Heros.ID
+AND Heros.ID = Arme.HerosID; 
+
+
